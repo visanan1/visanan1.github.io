77f10d3125044a9b9d6388bcfefa5633d305cd8c
visanan1 <visanan1@inf.upv.es>
2023-03-02 12:50:59 +0100

    Update Trabajo1.js

--- js/Trabajo1.js
+++ js/Trabajo1.js
@@ -32,7 +32,7 @@ function init() {
 
     // Instanciar la camara
     camera= new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,1,100);
-    camera.position.set(0.5,2,7);
+    camera.position.set(0.5,20,0);
     cameraControls = new OrbitControls( camera, renderer.domElement );
     cameraControls.target.set(0,1,0);
     camera.lookAt(0,1,0);
